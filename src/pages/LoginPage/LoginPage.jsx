f124c6549e740d07b7b334e32d08f87ab63b2bff
kostyaslob <kostyaslob@gmail.com>
2025-06-09 22:31:34 +0300

    update ContactsPage

---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,11 @@
+import PageTitle from "../../components/PageTitle/PageTitle";
+import LoginForm from "../../components/LoginForm/LoginForm";
+
+export default function LoginPage() {
+  return (
+    <div>
+      <PageTitle>Please log in</PageTitle>
+      <LoginForm />
+    </div>
+  );
+}
