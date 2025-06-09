cd683c166a7c545973218cae292a2dbee01d25e0
kostyaslob <kostyaslob@gmail.com>
2025-06-09 22:44:23 +0300

    update redux folders and files

---
+++ src/redux/contacts/slice.js
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { fetchContacts, deleteContact, addContact } from "./operations";
+
+const slice = createSlice({
+    name: "contacts",
+    initialState: {
+        items: [],
+        loading: false,
+        error: null
+    },
+    extraReducers: (builder) => builder.addCase(fetchContacts.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+    }).addCase(fetchContacts.fulfilled, (state, action) => {
+        state.loading = false;
+        state.items = action.payload;
+    }).addCase(fetchContacts.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+    }).addCase(deleteContact.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+    }).addCase(deleteContact.fulfilled, (state, action) => {
+        state.loading = false;
+        state.items = state.items.filter(item => item.id !== action.payload.id)
+    }).addCase(deleteContact.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+    }).addCase(addContact.pending, (state) => {
+        state.loading = true;
+    }).addCase(addContact.fulfilled, (state, action) => {
+        state.loading = false;
+        state.items.push(action.payload)
+    }).addCase(addContact.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+    })
+})
+
+export default slice.reducer;
